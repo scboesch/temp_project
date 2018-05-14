ee0b893e2217f8158bfa92a64092ed1d833f992f
Chris Boesch <ChrisBoesch@nus.edu.sg>
2018-05-14 14:01:14 +0800

    Updating project.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const styles = {
 const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
-    <h2>First project saved back to Github.. {"\u2728"}</h2>
+    <h2>First edit saved back to Github.. {"\u2728"}</h2>
   </div>
 );
 
